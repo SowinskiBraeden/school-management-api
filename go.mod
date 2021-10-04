b069ee1aa2f1b6377039fa88b40d3540f96e52de
Braeden57 <sowinskibraeden@gmail.com>
2021-10-04 12:15:13 -0700

    Connect to mongo

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module school-management
 
 go 1.16
 
-require go.mongodb.org/mongo-driver v1.7.2
+require (
+	github.com/joho/godotenv v1.3.0
+	go.mongodb.org/mongo-driver v1.7.2
+)
--- main.go
+++ main.go
@@ -1,15 +1,50 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
 	"school-management/models"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	var student models.Student
-	student.FirstName = "Braeden"
-	student.LastName = "Sowinski"
-	student.Age = 16
-	student.DOB = "2005-04-26" //year-month-day
-	fmt.Print(student)
+	godotenv.Load(".env")
+	mongoURI := os.Getenv("mongoURI")
+	dbo := os.Getenv("dbo")
+
+	// Connect to mongo
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+
+	// Get collection
+	collection := client.Database(dbo).Collection("students")
+	// res, insertErr := collection.InsertOne(ctx, student)
+	// if insertErr != nil {
+	// 	log.Fatal(insertErr)
+	// }
+	// fmt.Println(res)
+
+	cur, currErr := collection.Find(ctx, bson.D{})
+	if currErr != nil {
+		panic(currErr)
+	}
+	defer cur.Close(ctx)
+
+	var students []models.Student
+	if err = cur.All(ctx, &students); err != nil {
+		panic(err)
+	}
+	fmt.Println(students)
 }

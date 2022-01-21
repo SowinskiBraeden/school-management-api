9534d2f1f2d4481905292f2492b1c8f664abe770
Braeden Sowinski <sowinskibraeden@gmail.com>
2022-01-20 18:03:08 -0800

    update/remove-derprecated-code

--- routes/routes.go
+++ routes/routes.go
@@ -6,11 +6,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-type Person struct {
-	Name  string `json:"name" form:"name"`
-	Email string `json:"email" form:"email"`
-}
-
 func Setup(app *fiber.App) {
 	// API Handling
 

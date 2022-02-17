857981510a32b94be9bbf384648065c0d38387b8
Braeden Sowinski <sowinskibraeden@gmail.com>
2022-02-16 17:09:43 -0800

    simple/update/course model

--- models/courseModel.go
+++ models/courseModel.go
@@ -7,11 +7,12 @@ import (
 )
 
 type Course struct {
-	ID          primitive.ObjectID `bson:"_id"`
-	Name        string             `json:"name" validate:"required"`
-	Description string             `json:"description"`
-	Grade_level int                `json:"grade_level" validate:"required"`
-	Credits     int                `json:"credits" validate:"required"`
-	Created_at  time.Time          `json:"created_at"`
-	Updated_at  time.Time          `json:"updated_at"`
+	ID           primitive.ObjectID `bson:"_id"`
+	Name         string             `json:"name" validate:"required"`
+	Code         string             `json:"code" validate:"required"` // Short string to identify each class
+	GradeLevel   int                `json:"gradelevel" validate:"required"`
+	Credits      int                `json:"credits" default:"4"`
+	TotalRequest int                `json:"totalRequest" default:"0"` // Amount of requests for this course
+	Created_at   time.Time          `json:"created_at"`
+	Updated_at   time.Time          `json:"updated_at"`
 }
